2e8b8975d773a6281d9e22a44b24eb3fb4541119
Bolotbek <bolotbek.muratov@gmail.com>
2023-03-30 13:53:27 +0600

    last changes 8575-658

--- src/pages/Contacts/Contucts.js
+++ src/pages/Contacts/Contucts.js
@@ -1,27 +1,41 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import css from "./Contucts.module.scss";
-import { SampleMap } from "./Map/SampleMap";
+import { Map, YMaps, Placemark } from "react-yandex-maps";
+
+// import { SampleMap } from "./Map/SampleMap";
 
 const Contucts = () => {
   const { t } = useTranslation();
-  
-  const contactData = [
-    {
-      country: "ContactsKazahstan.country",
-      adress: "ContactsKazahstan.adress",
-      phone: "+7 771 225 98 34",
-      email: "alexandr.m@dolon.tech",
-      coordinates: [43.216977, 76.905598],
-    },
-    {
-      country: "ContactsKyrgyzstan.country",
-      adress: "ContactsKyrgyzstan.adress",
-      phone: "+996 700 173 295",
-      email: "satar.t@dolon.tech",
-      coordinates: [42.843686, 74.590648],
-    },
-  ];
+
+  const defaultPropsKz = {
+    center: [43.216977, 76.905598],
+    zoom: 13,
+  };
+  const defaultPropsKg = {
+    center: [42.843686, 74.590648],
+    zoom: 13,
+  };
+  const apiKey = "a7031374-99ea-465c-a189-62507eb3f61a";
+  const mapOptions = {
+    suppressMapOpenBlock: true,
+    lang: "en_US",
+  };
+
+  let kz = {
+    country: "ContactsKazahstan.country",
+    adress: "ContactsKazahstan.adress",
+    phone: "+7 771 225 98 34",
+    email: "alexandr.m@dolon.tech",
+    coordinates: [43.216977, 76.905598],
+  };
+  let kg = {
+    country: "ContactsKyrgyzstan.country",
+    adress: "ContactsKyrgyzstan.adress",
+    phone: "+996 700 173 295",
+    email: "satar.t@dolon.tech",
+    coordinates: [42.843686, 74.590648],
+  };
 
   return (
     <div className={css.contucts}>
@@ -29,21 +43,52 @@ const Contucts = () => {
         <div className={css.box}>
           <h3 className={css.title}>{t("Glob.Contacts")}</h3>
           <ul className={css.contactsUl}>
-            {contactData.map((item, index) => {
-              return (
-                <li key={index}>
-                  <p>{t(item.country)}</p>
-                  <p>{t(item.adress)}</p>
-                  <a className={css.phone} href={`tel:${item.phone}`}>
-                    {item.phone}
-                  </a>
-                  <a className={css.email} href={`mailto:${item.email}`}>
-                    {item.email}
-                  </a>
-                  <SampleMap city={item.coordinates} />
-                </li>
-              );
-            })}
+            <li>
+              <p>{t(kz.country)}</p>
+              <p>{t(kz.adress)}</p>
+              <a className={css.phone} href={`tel:${kz.phone}`}>
+                {kz.phone}
+              </a>
+              <a className={css.email} href={`mailto:${kz.email}`}>
+                {kz.email}
+              </a>
+              <div className={css.mapBox}>
+                <YMaps query={{ apikey: apiKey }}>
+                  <Map
+                    defaultState={defaultPropsKz}
+                    options={mapOptions}
+                    width="100%"
+                    height="100%"
+                  >
+                    <Placemark geometry={[43.216977, 76.905598]} />
+                  </Map>
+                </YMaps>
+              </div>
+              {/* <SampleMap city={kz.coordinates} /> */}
+            </li>
+            <li>
+              <p>{t(kg.country)}</p>
+              <p>{t(kg.adress)}</p>
+              <a className={css.phone} href={`tel:${kg.phone}`}>
+                {kg.phone}
+              </a>
+              <a className={css.email} href={`mailto:${kg.email}`}>
+                {kg.email}
+              </a>
+              <div className={css.mapBox}>
+                <YMaps query={{ apikey: apiKey }}>
+                  <Map
+                    defaultState={defaultPropsKg}
+                    options={mapOptions}
+                    width="100%"
+                    height="100%"
+                  >
+                    <Placemark geometry={[42.843686, 74.590648]} />
+                  </Map>
+                </YMaps>
+              </div>
+              {/* <SampleMap city={kg.coordinates} /> */}
+            </li>
           </ul>
         </div>
         <div className={css.locations}></div>
--- src/pages/Contacts/Map/SampleMap.js
+++ src/pages/Contacts/Map/SampleMap.js
@@ -1,30 +1,30 @@
-import React from "react";
-import { Map, YMaps, Placemark } from "react-yandex-maps";
-import css from "./SampleMap.module.scss";
+// import React from "react";
+// import { Map, YMaps, Placemark } from "react-yandex-maps";
+// import css from "./SampleMap.module.scss";
 
-export const SampleMap = ({ city }) => {
-  const defaultProps = {
-    center: city,
-    zoom: 13,
-  };
-  const apiKey = "a7031374-99ea-465c-a189-62507eb3f61a";
-  const mapOptions = {
-    suppressMapOpenBlock: true,
-    lang: "en_US",
-  };
+// export const SampleMap = ({ city }) => {
+//   const defaultProps = {
+//     center: city,
+//     zoom: 13,
+//   };
+//   const apiKey = "a7031374-99ea-465c-a189-62507eb3f61a";
+//   const mapOptions = {
+//     suppressMapOpenBlock: true,
+//     lang: "en_US",
+//   };
 
-  return (
-    <div className={css.mapBox}>
-      <YMaps query={{ apikey: apiKey }}>
-        <Map
-          defaultState={defaultProps}
-          options={mapOptions}
-          width="100%"
-          height="100%"
-        >
-          <Placemark geometry={city} />
-        </Map>
-      </YMaps>
-    </div>
-  );
-};
+//   return (
+//     <div className={css.mapBox}>
+//       <YMaps query={{ apikey: apiKey }}>
+//         <Map
+//           defaultState={defaultProps}
+//           options={mapOptions}
+//           width="100%"
+//           height="100%"
+//         >
+//           <Placemark geometry={city} />
+//         </Map>
+//       </YMaps>
+//     </div>
+//   );
+// };
